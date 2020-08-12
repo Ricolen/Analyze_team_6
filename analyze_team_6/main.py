290bd66891cf9a24ef7a55d9cf058fd6534f77fd
MoepeK <k.moepe@gmail.com>
2020-08-12 12:15:56 +0200

    Function 5
    Added Function 5 to the new Repository.

--- analyze_team_6/main.py
+++ analyze_team_6/main.py
@@ -147,9 +147,17 @@ def extract_municipality_hashtags(df):
 #Function_5
 
 ### START FUNCTION
+""""This code serves to calculate the number of tweets per day, 
+    taking in a pandas dataframe as input and returning a new dataframe grouped by day and number of tweets """
 def number_of_tweets_per_day(df):
     # your code here
-    return
+    # We slice the portion of the code needed.
+    df['Date'] = [date[0:10] for date in df['Date']]
+    # Drop the duplicated column.
+    new_df = df.drop(columns=['Date'],axis=1)
+    # Group the contents of the column by the sliced date.
+    new_df = new_df.groupby(df['Date']).count() 
+    return new_df
 
 ### END FUNCTION
 
